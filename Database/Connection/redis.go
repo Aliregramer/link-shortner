2b2064ca528b3ff3ba492e8eadbd11f47e15c971
alireza alizadeh <alirezaalizadeh025@gmail.com>
2022-12-28 12:13:18 +0330

    not load .env file

--- Database/Connection/redis.go
+++ Database/Connection/redis.go
@@ -4,11 +4,11 @@ import (
 	h "shorjiga/Helper"
 
 	"github.com/go-redis/redis"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func RedisClient() *redis.Client {
-	godotenv.Load()
+	// godotenv.Load()
 	return redis.NewClient(&redis.Options{
 		Addr:     h.Getenv("REDIS_HOST", "127.0.0.1") + ":" + h.Getenv("REDIS_PORT", "6379"),
 		Password: "",
